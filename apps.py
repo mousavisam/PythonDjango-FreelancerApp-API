31340566e52658d84821446a2b250cd98e0fa932
DeepSheth369 <124446137+DeepSheth369@users.noreply.github.com>
2023-04-12 23:35:48 -0400

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ChatConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.chat'
+    name = 'apps.user'
