27909f1b421c76c6184f2e6400ee8e5763cd7440
DeepSheth369 <124446137+DeepSheth369@users.noreply.github.com>
2023-04-12 23:36:04 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,21 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from shortuuidfield import ShortUUIDField
-from apps.user.models import User
 
-class ChatRoom(models.Model):
-	roomId = ShortUUIDField()
-	type = models.CharField(max_length=10, default='DM')
-	member = models.ManyToManyField(User)
-	name = models.CharField(max_length=20, null=True, blank=True)
+class User(AbstractUser):
+	userId = ShortUUIDField()
+	image = models.ImageField(upload_to="user")
 
-	def __str__(self):
-		return self.roomId + ' -> ' + str(self.name)
-
-class ChatMessage(models.Model):
-	chat = models.ForeignKey(ChatRoom, on_delete=models.SET_NULL, null=True)
-	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-	message = models.CharField(max_length=255)
-	timestamp = models.DateTimeField(auto_now_add=True)
+class OnlineUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return self.message
+		return self.user.username
